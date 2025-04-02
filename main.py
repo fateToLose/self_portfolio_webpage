1a8d67bdaa2f002a82ae3b08b1ceaa0b03cc1796
Kenneth <kenneth_wj@outlook.com>
2025-04-03 00:14:33 +0800

    added empty pages and improvised on landing page

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
+import os
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
+ref_path: str = os.path.dirname(__file__)
+
+
+def load_json_data(filename):
+    filepath = os.path.join(ref_path, "data", filename)
+    with open(filepath, "r") as file:
+        return json.load(file)
 
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    profile = load_json_data("profile.json")
+    return render_template("landing_page.html", profile=profile, active_page="home")
+
+
+@app.route("/portfolio")
+def portfolio_page():
+    return render_template("portfolio.html", active_page="portfolio")
+
+
+@app.route("/contact")
+def contact_me():
+    return render_template("contact_me.html", active_page="contact")
 
 
 if __name__ == "__main__":
---
+++ static/js/script.js
@@ -0,0 +1,21 @@
+// navigation buttons
+document.addEventListener('DOMContentLoaded', () => {
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    if (burger) {
+        burger.addEventListener('click', () => {
+            // Toggle navigation
+            nav.classList.toggle('nav-active');
+            
+            // Animate links
+            navLinks.forEach((link, index) => {
+                if (link.style.animation) {
+                    link.style.animation = '';
+                } else {
+                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+                }
+            });
+        }
+    }
+});
